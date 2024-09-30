0877ad4d13d22e59974a0188185c8ce50a9d001d
Akira_142120 <akira.higa14@gmail.com>
2024-09-30 14:44:42 -0300

    criação_main
    Criação da função main e das funções de carregar usuário e menu principal.

---
+++ main.c
@@ -0,0 +1,9 @@
+int main() {
+    int totalUsers = 0;
+    char users[100][100], cpfs[100][13], senhas[100][20];
+
+    carregar_usuarios(&totalUsers, users, cpfs, senhas);
+    menu_principal(&totalUsers, users, cpfs, senhas);
+
+    return 0;
+}
