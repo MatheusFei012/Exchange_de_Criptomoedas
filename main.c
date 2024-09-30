91e63c51c027d2c940dc33c7530ed48fc8a6332b
Akira_142120 <akira.higa14@gmail.com>
2024-09-30 15:54:03 -0300

    Criação da função salvar_usuario
    Salva em um arquivo binário os dados do usuário.

--- main.c
+++ main.c
@@ -3,6 +3,19 @@
 #include <string.h>
 #include <time.h>
 
+void salvar_usuarios(int totalUsers, char users[][100], char cpfs[][13], char senhas[][20]) {
+    FILE *file = fopen("usuarios.bin", "wb");
+    if (file) {
+        fwrite(&totalUsers, sizeof(int), 1, file);
+        fwrite(users, sizeof(char[100]), totalUsers, file);
+        fwrite(cpfs, sizeof(char[13]), totalUsers, file);
+        fwrite(senhas, sizeof(char[20]), totalUsers, file);
+        fclose(file);
+    } else {
+        printf("Erro ao salvar usuários.\n");
+    }
+}
+
 int main() {
     int totalUsers = 0;
     char users[100][100], cpfs[100][13], senhas[100][20];
