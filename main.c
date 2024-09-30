7ff55e8a989d2ab58d9335e708ce80fedac2e4c4
Akira_142120 <akira.higa14@gmail.com>
2024-09-30 16:43:45 -0300

    Criação das funções de menu, validação do cpf, pausa e limpar a tela.
    A função Menu tem as opções que o usuário pode escolher depois de realizar o login.
    
    A função valida cpf faz a verificação dos 11 digitos no cpf.
    
    A função pausa exibe uma mensagem para o usuário e última função limpa a tela.

--- main.c
+++ main.c
@@ -3,6 +3,32 @@
 #include <string.h>
 #include <time.h>
 
+void menu() {
+    printf("1. Consultar saldo\n");
+    printf("2. Consultar extrato\n");
+    printf("3. Depositar\n");
+    printf("4. Sacar\n");
+    printf("5. Comprar criptomoedas\n");
+    printf("6. Vender criptomoedas\n");
+    printf("7. Atualizar cotação\n");
+    printf("8. Sair\n");
+    printf("\nEscolha uma opção: ");
+}
+
+void limpar_tela() {
+    system("cls || clear");
+}
+
+void pausar() {
+    printf("\nPressione Enter para continuar...");
+    getchar();
+}
+
+int validar_cpf(const char *cpf) {
+    return strlen(cpf) == 11 && strspn(cpf, "0123456789") == 11;
+}
+
+
 int main() {
     int totalUsers = 0;
     char users[100][100], cpfs[100][13], senhas[100][20];
