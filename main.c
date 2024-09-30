358a33bf4e5550f98da3fa1abb2232811dd0fe50
Akira_142120 <akira.higa14@gmail.com>
2024-09-30 15:29:14 -0300

    Criação da função carregar usuário
    Essa função lê e armazena as informações sobre o usuário.

--- main.c
+++ main.c
@@ -3,6 +3,22 @@
 #include <string.h>
 #include <time.h>
 
+void carregar_usuarios(int *totalUsers, char users[][100], char cpfs[][13], char senhas[][20]){
+    FILE *file = fopen("usuarios.bin", "rb");
+    if(file){
+        fread(totalUsers, sizeof(int), 1, file);
+        fread(users, sizeof(char[100]), *totalUsers, file);
+        fread(cpfs, sizeof(char[13]), *totalUsers, file);
+        fread(senhas, sizeof(char[20]), *totalUsers, file);
+        fclose(file);
+    }
+    else{
+        printf("Erro ao abrir o arquivo de usuários.\n");
+    }
+}
+
+
+
 int main() {
     int totalUsers = 0;
     char users[100][100], cpfs[100][13], senhas[100][20];
