142ad283ce89c838f80bef43a2920b9ec96f2de2
MatheusFei012 <mcruz20070151@gmail.com>
2024-10-01 16:23:10 -0300

    Função menu_principal
    O menu principal serve para que o cliente consiga, acessar o menu, fazer seu login, se registrar, comprar kriptomoedas, consultar seu saldo, praticamente tudo, sendo uma das partes mais importantes do código.

--- main.c
+++ main.c
@@ -38,3 +38,81 @@ int main() {
 
     return 0;
 }
+
+void menu_principal(int totalUsers, char users[][100], char cpfs[][13], char senhas[][20]) {
+    while (1) {
+        printf("Menu:\n");
+        printf("1. Login\n");
+        printf("2. Registrar\n");
+        printf("3. Sair\n");
+        printf("Escolha uma opção: ");
+
+        int opcao;
+        scanf("%d", &opcao);
+        getchar(); // Limpa o buffer
+
+        switch (opcao) {
+            case 1: {
+                int usuarioIndex = login(totalUsers, users, cpfs, senhas);
+                if (usuarioIndex < 0) {
+                    continue;
+                }
+
+                float consaldo, bitcoin, ethereum, ripple;
+                carregar_saldo(&consaldo, &bitcoin, &ethereum, &ripple, usuarioIndex);
+
+                while (1) {
+                    menu();
+                    scanf("%d", &opcao);
+                    getchar(); // Limpa o buffer
+
+                    switch (opcao) {
+                        case 1:
+                            saldo(consaldo, bitcoin, ethereum, ripple);
+                            break;
+                        case 2:
+                            carregar_extrato(usuarioIndex);
+                            break;
+                        case 3:
+                            depositar(&consaldo);
+                            salvar_saldo(consaldo, bitcoin, ethereum, ripple, usuarioIndex);
+                            break;
+                        case 4:
+                            sacar(&consaldo);
+                            salvar_saldo(consaldo, bitcoin, ethereum, ripple, usuarioIndex);
+                            break;
+case 5:
+                            comprar_criptomoeda(&consaldo, &bitcoin, &ethereum, &ripple, usuarioIndex);
+                            salvar_saldo(consaldo, bitcoin, ethereum, ripple, usuarioIndex);
+                            break;
+                        case 6:
+                            vender_criptomoeda(&consaldo, &bitcoin, &ethereum, &ripple, usuarioIndex);
+                            salvar_saldo(consaldo, bitcoin, ethereum, ripple, usuarioIndex);
+                            break;
+                        case 7:
+                            atualizar_cotacao();
+                            break;
+                        case 8:
+                            printf("Saindo...\n");
+                            return;
+                        default:
+                            printf("Opção inválida. Tente novamente.\n");
+                            pausar();
+                            break;
+                    }
+                }
+                break;
+            }
+            case 2:
+                registrar(totalUsers, users, cpfs, senhas);
+                break;
+            case 3:
+                printf("Saindo...\n");
+                return;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+                pausar();
+                break;
+        }
+    }
+}
