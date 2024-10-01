7d6904d75d2b51637e609bfb8978f316df47e242
JB <160656573+JJB05@users.noreply.github.com>
2024-09-30 22:24:01 -0300

    Menu de Saldo
    A função saldo mostra de forma formatada o saldo do usuário.

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 #include <string.h>
 #include <time.h>
 
+void saldo(float consaldo, float bitcoin, float ethereum, float ripple) {
+    limpar_tela();
+    printf("Saldo disponível: R$ %.2f\n", consaldo);
+    printf("Bitcoin: %.6f BTC\n", bitcoin);
+    printf("Ethereum: %.6f ETH\n", ethereum);
+    printf("Ripple: %.6f XRP\n", ripple);
+    pausar();
+    limpar_tela();
+}
+
 int main() {
     int totalUsers = 0;
     char users[100][100], cpfs[100][13], senhas[100][20];
