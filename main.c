5d6541b88dbc27b2d9520859867c8c24307775f9
JB <160656573+JJB05@users.noreply.github.com>
2024-09-30 22:15:07 -0300

    Carregar e salvar saldo
    Função carregarsaldo: Lê os saldos dos usuários  de um arquivo específico, identificado pelo índice. O arquivo é aberto, os valores são lidos e, em seguida, o arquivo é fechado.
    
    Função salvarsaldo: Salva os saldos dos usuários em arquivo binário, também baseado no índice. Os dados são escritos no arquivo e depois o arquivo é fechado.

--- main.c
+++ main.c
@@ -3,6 +3,36 @@
 #include <string.h>
 #include <time.h>
 
+void carregarsaldo(float consaldo, floatbitcoin, float ethereum, floatripple, int usuarioIndex) {
+    char filename[50];
+    snprintf(filename, sizeof(filename), "saldo%d.bin", usuarioIndex);
+    FILE file = fopen(filename, "rb");
+    if (file) {
+        fread(consaldo, sizeof(float), 1, file);
+        fread(bitcoin, sizeof(float), 1, file);
+        fread(ethereum, sizeof(float), 1, file);
+        fread(ripple, sizeof(float), 1, file);
+        fclose(file);
+    } else {
+        printf("Erro ao abrir o arquivo de saldo.\n");
+    }
+}
+
+void salvarsaldo(float consaldo, float bitcoin, float ethereum, float ripple, int usuarioIndex) {
+    char filename[50];
+    snprintf(filename, sizeof(filename), "saldo%d.bin", usuarioIndex);
+    FILEfile = fopen(filename, "wb");
+    if (file) {
+        fwrite(&consaldo, sizeof(float), 1, file);
+        fwrite(&bitcoin, sizeof(float), 1, file);
+        fwrite(&ethereum, sizeof(float), 1, file);
+        fwrite(&ripple, sizeof(float), 1, file);
+        fclose(file);
+    } else {
+        printf("Erro ao salvar saldo.\n");
+    }
+}
+
 int main() {
     int totalUsers = 0;
     char users[100][100], cpfs[100][13], senhas[100][20];
