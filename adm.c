60a2369c4e020a36d8290d908298e05a368f5a21
MatheusFei012 <mcruz20070151@gmail.com>
2024-11-19 01:07:31 -0300

    Função de login
    Essa função serve para logar em nosso programa, nela a diversas operações para garantir se a pessoa esta logando de maneira correta.

--- adm.c
+++ adm.c
@@ -24,3 +24,41 @@ void pausar() {
     printf("\nPressione Enter para continuar...");
     getchar();  // Espera o usuário pressionar Enter
 }
+
+// Função de login do administrador
+int login_administrador() {
+    char cpf[13], senha[20];
+
+    limpar_tela();
+    printf("Digite o CPF do administrador: ");
+    fgets(cpf, sizeof(cpf), stdin);
+    strtok(cpf, "\n");
+
+    if (strcmp(cpf, ADMIN_CPF) != 0) {
+        printf("CPF incorreto.\n");
+        pausar();
+        return 0;
+    }
+
+    printf("Digite a senha do administrador: ");
+    fgets(senha, sizeof(senha), stdin);
+    strtok(senha, "\n");
+
+    if (strcmp(senha, ADMIN_SENHA) != 0) {
+        printf("Senha incorreta.\n");
+        pausar();
+        return 0;
+    }
+
+    printf("Login realizado com sucesso!\n");
+    pausar();
+    return 1;  // Login bem-sucedido
+}
+
+// Função para cadastrar um novo investidor
+typedef struct {
+    char nome[100];
+    char cpf[13];
+    char senha[20];
+} Investidor;
+
